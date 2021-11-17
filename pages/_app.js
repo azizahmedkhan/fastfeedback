84bc5ec600017e76e23966a2b0e3af1ee84d1112
azizahmedkhan <azizahmed.khan@gmail.com>
2021-11-17 17:54:03 +1300

    first change

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import { AuthProvider } from '@/lib/auth'
+import '@/styles/globals.css'
+import theme from '@/styles/theme';
+import { ChakraProvider } from "@chakra-ui/react"
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+    <AuthProvider>
+       <Component {...pageProps} />
+    </AuthProvider>
+  </ChakraProvider>
+  );
+    
 }
 
 export default MyApp
